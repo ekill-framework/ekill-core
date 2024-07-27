9c3c2d0925b86a5f32d8f3941c1646b74f24e0e3
eKill-Framework <ekill.framework@gmail.com>
2024-07-27 08:43:55 +0200

    fix(core): return type check IsDebugEnabled, IsLogEnabled, GetPrefix

--- plugins/ekill-core/main.lua
+++ plugins/ekill-core/main.lua
@@ -1,10 +1,10 @@
 if not Core then Core = {} end
 
-Core.IsDebugEnabled = function() return config:Fetch("ekill-core.debug") end
+Core.IsDebugEnabled = function() return (config:Fetch("ekill-core.debug") == true) end
 
-Core.IsLogEnabled = function() return config:Fetch("ekill-core.log") end
+Core.IsLogEnabled = function() return (config:Fetch("ekill-core.log") == true) end
 
-Core.GetPrefix = function() return config:Fetch("ekill-core.prefix") end
+Core.GetPrefix = function() return config:Fetch("ekill-core.prefix") or nil end
 
 AddEventHandler("OnPluginStart", function(event)
    print(string.format("Starting {PURPLE}v%s{DEFAULT}", GetPluginVersion()))
