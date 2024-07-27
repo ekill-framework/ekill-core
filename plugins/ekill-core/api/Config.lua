f185a193170f0528794033e59832c89c1938c44f
eKill-Framework <ekill.framework@gmail.com>
2024-07-27 08:30:16 +0200

    add(api): adding comments to functions

--- plugins/ekill-core/api/Config.lua
+++ plugins/ekill-core/api/Config.lua
@@ -3,10 +3,12 @@ if not Core.API then Core.API = {} end
 
 Core.API.Config = {}
 
----comment
----@param configName any
+---Load config from name file
+---File.json should be placed on addons/swiftly/configs/plugins/ekill/
+---@param configName string
 ---@return table|nil
 function Core.API.Config:Get(configName --[[string]])
     return Core.ConfigManager:Get(configName)
 end
+
 
